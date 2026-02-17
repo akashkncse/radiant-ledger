d8679490536865358fefbb2b31538387074f08bb
Akash K N <akashkncse@gmail.com>
2026-02-17 16:06:59 +0530

    feat: Add queries for creation of transaction table

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,5 +1,14 @@
-CREATE TABLE account (
+CREATE TABLE IF NOT EXISTS account (
     accountID varchar(255) PRIMARY KEY,
-    password_hash varchar(255) not null,
+    passwordHash varchar(255) not null,
     balance numeric(18, 2) not null default 0.00
 );
+
+CREATE TABLE IF NOT EXISTS transaction (
+    transactionID varchar(255) PRIMARY KEY,
+    fromAccountID varchar(255) not null,
+    toAccountID varchar(255) not null,
+    amount numeric(18, 2) not null default 0.00,
+    type varchar(255) not null,
+    timestamp timestamp default current_timestamp
+)
