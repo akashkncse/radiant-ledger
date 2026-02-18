611683c7983ab7dbb044d431cc01c7c72a37ef4e
Akash K N <akashkncse@gmail.com>
2026-02-18 20:29:08 +0530

    refactor: remove static from DatabaseManager to support dependency injection

--- src/main/java/com/akashkn/radiantledger/db/DatabaseManager.java
+++ src/main/java/com/akashkn/radiantledger/db/DatabaseManager.java
@@ -7,7 +7,7 @@ public class DatabaseManager {
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/radiantledger";
     private static final String DB_USER = "postgres";
     private static final String DB_PASSWORD = "123";
-    public static Connection getConnection() throws SQLException{
+    public Connection getConnection() throws SQLException{
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 }
