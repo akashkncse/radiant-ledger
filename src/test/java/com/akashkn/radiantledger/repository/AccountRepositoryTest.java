67668f5eb4acf28fc164b5b4971c11f5d44fb03f
Akash K N <akashkncse@gmail.com>
2026-02-23 22:34:08 +0530

    test: add findById() test for AccountRepository

--- src/test/java/com/akashkn/radiantledger/repository/AccountRepositoryTest.java
+++ src/test/java/com/akashkn/radiantledger/repository/AccountRepositoryTest.java
@@ -1,20 +1,24 @@
 package com.akashkn.radiantledger.repository;
 
+import com.akashkn.radiantledger.db.DatabaseManager;
+import com.akashkn.radiantledger.model.Account;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountRepositoryTest {
 
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine").withInitScript("db/schema.sql");
 
     AccountRepository accountRepository;
 
@@ -31,7 +35,22 @@ class AccountRepositoryTest {
     @BeforeEach
     void setUp()
     {
+        DatabaseManager db = new DatabaseManager(
+                postgres.getJdbcUrl(),
+                postgres.getUsername(),
+                postgres.getPassword()
+        );
+        accountRepository = new AccountRepository(db);
+    }
 
+    @Test
+    void testFindByID()
+    {
+        accountRepository.save(new Account("A1", new BigDecimal("10.23"), "lsdjf"));
+        accountRepository.save(new Account("B2", new BigDecimal("12.23"), "dsfsdfsdf"));
+
+        Optional<Account> acc = accountRepository.findByID("B2");
+        assertEquals("B2", acc.get().getAccountID());
     }
 
 }
