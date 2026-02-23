54876b5afc03584eb38caa627a6eeba70da7bf92
Akash K N <akashkncse@gmail.com>
2026-02-23 22:32:53 +0530

    refactor: inject database configuration via constructor

--- src/main/java/com/akashkn/radiantledger/db/DatabaseManager.java
+++ src/main/java/com/akashkn/radiantledger/db/DatabaseManager.java
@@ -4,9 +4,15 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 
 public class DatabaseManager {
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/radiantledger";
-    private static final String DB_USER = "postgres";
-    private static final String DB_PASSWORD = "123";
+    private final String DB_URL;
+    private final String DB_USER;
+    private final String DB_PASSWORD;
+    public DatabaseManager(String DB_URL, String DB_USER, String DB_PASSWORD)
+    {
+        this.DB_URL = DB_URL;
+        this.DB_USER = DB_USER;
+        this.DB_PASSWORD = DB_PASSWORD;
+    }
     public Connection getConnection() throws SQLException{
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
