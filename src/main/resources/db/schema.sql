4a9614539ec0f6abfd92febc2a192eb9a28002b2
Akash K N <akashkncse@gmail.com>
2026-02-23 22:31:05 +0530

    refactor: refactor table names to snake case

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS account (
-    accountID varchar(255) PRIMARY KEY,
-    passwordHash varchar(255) not null,
+    account_id varchar(255) PRIMARY KEY,
+    password_hash varchar(255) not null,
     balance numeric(18, 2) not null default 0.00
 );
 
 CREATE TABLE IF NOT EXISTS transaction (
-    transactionID varchar(255) PRIMARY KEY,
-    fromAccountID varchar(255) not null,
-    toAccountID varchar(255) not null,
+    transaction_id varchar(255) PRIMARY KEY,
+    from_account_id varchar(255) not null,
+    to_account_id varchar(255) not null,
     amount numeric(18, 2) not null default 0.00,
     type varchar(255) not null,
     timestamp timestamp default current_timestamp
