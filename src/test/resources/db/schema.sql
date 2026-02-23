358cdb717936b57491e0849cea9596c40589e6e6
Akash K N <akashkncse@gmail.com>
2026-02-23 22:31:28 +0530

    feat: add schema.sql for test database

---
+++ src/test/resources/db/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS account (
+    account_id varchar(255) PRIMARY KEY,
+    password_hash varchar(255) not null,
+    balance numeric(18, 2) not null default 0.00
+);
+
+CREATE TABLE IF NOT EXISTS transaction (
+    transaction_id varchar(255) PRIMARY KEY,
+    fromAccount_id varchar(255) not null,
+    toAccount_id varchar(255) not null,
+    amount numeric(18, 2) not null default 0.00,
+    type varchar(255) not null,
+    timestamp timestamp default current_timestamp
+)
