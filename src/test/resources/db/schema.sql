d3ecb4c2c689ce4d65714ed325ac255cca90a1b6
Akash K N <akashkncse@gmail.com>
2026-02-24 09:39:43 +0530

    fix: fix column name mismatch in schema.sql

--- src/test/resources/db/schema.sql
+++ src/test/resources/db/schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS account (
 
 CREATE TABLE IF NOT EXISTS transaction (
     transaction_id varchar(255) PRIMARY KEY,
-    fromAccount_id varchar(255) not null,
-    toAccount_id varchar(255) not null,
+    from_account_id varchar(255) not null,
+    to_account_id varchar(255) not null,
     amount numeric(18, 2) not null default 0.00,
     type varchar(255) not null,
     timestamp timestamp default current_timestamp
